1a58e6ce416213aceefb169a2cf05fe1230ccfaa
Joey Patrum (RIT Student) <jdp1701@g.rit.edu>
2025-04-16 11:52:07 -0400

    Added some boilerplate

---
+++ server/router.js
@@ -0,0 +1,8 @@
+const controllers = require('./controllers');
+const mid = require('./middleware');
+
+const router = (app) => {
+    // TODO
+};
+
+module.exports = router;
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
     entry: {
+        login: './client/login.jsx',
     },
     module: {
         rules: [
