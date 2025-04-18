cbb0c77cbda093ea2343268abf7a46a464fde19a
Joey Patrum (RIT Student) <jdp1701@g.rit.edu>
2025-04-18 12:07:01 -0400

    Set up login functionality

--- server/router.js
+++ server/router.js
@@ -2,7 +2,12 @@ const controllers = require('./controllers');
 const mid = require('./middleware');
 
 const router = (app) => {
-  // TODO
+  app.get('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
+  app.post('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.login);
+
+  app.post('/signup', mid.requiresSecure, mid.requiresLogout, controllers.Account.signup);
+
+  app.get('/logout', mid.requiresLogin, controllers.Account.logout);
 };
 
 module.exports = router;
